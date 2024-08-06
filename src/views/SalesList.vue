2611d4f998762228aa1315da14e0efe77f141de7
Namani31 <kwg531@naver.com>
2024-08-06 10:40:47 +0900

    Feat: 라우터에 레이아웃 설정

---
+++ src/views/SalesList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="">
+    SaleList
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SaleList',
+  components: {
+    
+  },
+  data() {
+    return {
+      
+    };
+  },
+  setup() {
+    
+  },
+  created() {
+    
+  },
+  mounted() {
+    
+  },
+  unmounted() {
+    
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
