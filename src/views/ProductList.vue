63dd460ce41d3a4b9e387e0a252b75cfc33e701f
Namani31 <kwg531@naver.com>
2024-08-06 14:01:10 +0900

    Feat: 카테고리 선택박스 생성

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,38 +1,25 @@
 <template>
-  <div class="">
-    ProductList
-  </div>
+  <main class="mt-3">
+    <div class="container">
+      <div class="col-12">
+        <!-- 카테고리 선택박스 -->
+        <select class="form-select" id="">
+          <option value="">선택하세요</option>
+          <option value="노트북">노트북</option>
+          <option value="모니터">모니터</option>
+          <option value="PC용품">PC용품</option>
+        </select>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'ProductList',
-  components: {
-    
-  },
-  data() {
-    return {
-      
-    };
-  },
-  setup() {
-    
-  },
-  created() {
-    
-  },
-  mounted() {
-    
-  },
-  unmounted() {
-    
-  },
-  methods: {
-    
-  }
+  name: "ProductList",
+  components: {},
+  methods: {},
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
